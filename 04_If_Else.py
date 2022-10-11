33bf075fcaadbf6c778cde62779806db6c6abe72
Vipul Goswami <vipul.jagdishchandra.goswami@schibsted.com>
2022-10-11 14:46:07 +0200

    If condition in python

--- 04_If_Else.py
+++ 04_If_Else.py
@@ -4,6 +4,9 @@
 # if both then FizzBuzz 
 # else number it self.
 
+from sre_parse import fix_flags
+
+
 class FizzBuzz:
 
     def first_function(self):
@@ -29,6 +32,21 @@ class FizzBuzz:
                 print('Buzz')
             else:
                 print(n)
+    
+    def third_function(self):
+        n = 3
+        print('Fizz' if n % 3 == 0 else n)
+
+        n = 5
+        print('Buzz' if n % 5 == 0 else n)
+
+        # This is ternary operator in Python, but its recommended to use the 'elif' instead of this as this is hard for readabilty purpose.
+        print(['FizzBuzz' if n % 15 == 0 else 'Fizz' if n % 3 == 0 else 'Buzz' if n % 5 ==0 else n for n in range(1,101)])
 
 fizzBuzz = FizzBuzz()
+print('First Function')
+fizzBuzz.first_function()
+print('Second Function')
 fizzBuzz.second_function()
+print('Third Function')
+fizzBuzz.third_function()
