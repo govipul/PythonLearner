c1a3eb31a5cb398b496b2333f67d6305100f7029
Vipul Goswami <vipul.jagdishchandra.goswami@schibsted.com>
2022-10-14 14:33:11 +0200

    updated the static method

--- 05_02_Static_Method.py
+++ 05_02_Static_Method.py
@@ -3,13 +3,16 @@ import math
 
 
 class Orientations:
+    pi = 3.14
     def __init__(self, x_pos, y_pos, degree) -> None:
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.x_dir, self.y_dir = Orientations.getUnitVectorFromDegree(degree)
+        self.x_dir, self.y_dir = self.getUnitVectorFromDegree(degree)
 
-    def getUnitVectorFromDegree(degree) -> int:
-        radians = (degree/180) * math.pi
+    @staticmethod
+    def getUnitVectorFromDegree(degree) -> int: # <- This is how we can define the static method 
+        # radians = (degree/180) * math.pi
+        radians = (degree/180) * Orientations.pi # <- This is a static variable name pi
         return math.sin(radians), - math.cos(radians)
     
     def getNextPos(self) -> int:
