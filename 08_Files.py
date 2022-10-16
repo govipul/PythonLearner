0bf7c9e1398c4587c046697eca7653572e74b5f8
Vipul Goswami <vipul.jagdishchandra.goswami@schibsted.com>
2022-10-16 17:19:32 +0200

    Added the comment

--- 08_Files.py
+++ 08_Files.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
 
-# add the correct path
-print(os.getcwd())
 filrPath = os.getcwd() + '/PythonLearner/input/01_test.txt'
-with open(filrPath, 'r') as reader:
+with open(filrPath, 'r') as reader: # <- will close the file gracefully compared to reader = open(filrPath, 'r')
     for line in reader.readlines():
         print(line.strip())
