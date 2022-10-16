c284155e09bc8b5ba1995e552696cb529f57617e
Vipul Goswami <vipul.jagdishchandra.goswami@schibsted.com>
2022-10-16 17:26:05 +0200

    Added the file read,write and append

--- 08_Files.py
+++ 08_Files.py
@@ -1,7 +1,16 @@
 import os
 from pathlib import Path
 
-filrPath = os.getcwd() + '/PythonLearner/input/01_test.txt'
-with open(filrPath, 'r') as reader: # <- will close the file gracefully compared to reader = open(filrPath, 'r')
+filePath = os.getcwd() + '/PythonLearner/input/01_test.txt'
+with open(filePath, 'r') as reader: # <- will close the file gracefully compared to reader = open(filrPath, 'r')
     for line in reader.readlines():
         print(line.strip())
+
+writeFilePath = filePath = os.getcwd() + '/PythonLearner/input/02_write.txt'
+with open(writeFilePath, 'w') as writer:
+        writer.write('This is a line 1\n')
+        writer.write('This is a line 2\n')
+
+writeFilePath = filePath = os.getcwd() + '/PythonLearner/input/02_write.txt'
+with open(writeFilePath, 'a') as writer:
+        writer.write('append this line 3\n')
